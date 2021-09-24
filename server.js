42df57dedf2d6678c11ed5dbcce118332b01fc9e
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-09-23 21:17:30 -0400

    table and styling done

--- server.js
+++ server.js
@@ -3,21 +3,35 @@ const budget = require('./models/budget');
 const app = express();
 PORT = 3000;
 
+// Static
+app.use(express.static('public'));
+
 //DATABASE
 const budgets = require('./models/budget');
 
 //INDEX
 app.get('/budgets', (req, res) => {
-    res.render('index.ejs', {allBudgets: budgets});
+    res.render('index.ejs', { allBudgets: budgets });
 });
+
+//NEW
+app.get('/budgets/new', (req, res) => {
+            res.render('new.ejs');
+});
+
+//CREATE
+app.post('/budgets', (req, res) => {
+            console.log('HI')
+        })
 
 //SHOW
 app.get('/budgets/:index', (req, res) => {
-    res.render('show.ejs', {budgets: budgets[req.params.index]})
-})
+            res.render('show.ejs', { budgets: budgets[req.params.index] });
+});
+
 
 
 //Express Web Server port - app.listen
 app.listen(PORT, () => {
-    console.log('listening')
-});
+            console.log('listening')
+        });
