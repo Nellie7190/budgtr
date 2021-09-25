769be20174b6eb44a745a55df93dffd7a37ead2f
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-09-24 22:04:01 -0400

    fully working and styled app, more is optional

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ app.use(express.static('public'));
 
 //DATABASE
 const budgets = require('./models/budget');
+let bankAccount = 0;
 
 //INDEX
 app.get('/budgets', (req, res) => {
-    res.render('index.ejs', { allBudgets: budgets });
+    // res.render(bankAccount);
+    res.render('index.ejs', { allBudgets: budgets, account: bankAccount});
 });
 
 //NEW
@@ -39,6 +41,7 @@ app.get('/budgets/:index', (req, res) => {
     res.render('show.ejs', { budgets: budgets[req.params.index] });
 });
 
+//
 
 
 //Express Web Server port - app.listen
